0905e522334e44035d5f937da21ba6deea25ef5f
Bryce Hill <bryceahill@gmail.com>
2015-09-19 09:03:17 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+This is just a prototype. Don't judge. Be kind.
+
+```
+npm install
+npm start
+```
+
+Since this is a prototype, using a fake json api using the json-server module
+```
+npm install -g json-server
+json-server --watch jsob/db.json
+```
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+    entry: [
+      'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+      'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+      './src/index.js' // Your appʼs entry point
+    ],
+
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/static/'
+    },
+
+    module: {
+      loaders: [{
+            test: /\.jsx?$/,
+            loaders: ['react-hot', 'babel'],
+            include: path.join(__dirname, 'src'),
+            exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    resolveLoader: {
+        root: path.join(__dirname, "node_modules")
+    }
+
+}
